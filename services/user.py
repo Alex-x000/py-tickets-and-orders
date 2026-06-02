177a786d4eacf8496b90d9bc5d354e7a4d433c11
Sasha <alex.acmate@gmail.com>
2026-06-02 23:01:10 +0300

    Solution

--- services/user.py
+++ services/user.py
@@ -2,8 +2,12 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-def create_user(username: str, password: str, email: str = None,
-                first_name: str = None, last_name: str = None) -> User:
+def create_user(username: str,
+                password: str,
+                email: str = None,
+                first_name: str = None,
+                last_name: str = None
+                ) -> User:
     user = User.objects.create_user(username=username,
                                     password=password,
                                     email=email,
